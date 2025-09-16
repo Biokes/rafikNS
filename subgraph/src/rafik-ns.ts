b5470dfebee97f1730c42602b2e6a5b1c53c81c1
Biokes <106444765+Biokes@users.noreply.github.com>
2025-09-16 21:28:51 +0100

    update schema.graphql

--- subgraph/src/rafik-ns.ts
+++ subgraph/src/rafik-ns.ts
@@ -1,35 +1,80 @@
 import {
   CreatedName as CreatedNameEvent,
-  Messaging as MessagingEvent
+  Messaging as MessagingEvent,
 } from "../generated/RafikNS/RafikNS"
-import { CreatedName, Messaging } from "../generated/schema"
+import { ethereum } from "@graphprotocol/graph-ts"
+import {
+  CreatedName,
+  Messaging,
+  RafikNS,
+  Transaction,
+} from "../generated/schema"
+
+function getOrCreateTransaction(event: ethereum.Event): Transaction {
+  let txId = event.transaction.hash.toHexString()
+  let tx = Transaction.load(txId)
+  if (tx == null) {
+    tx = new Transaction(txId)
+    tx.blockNumber = event.block.number
+    tx.blockTimestamp = event.block.timestamp
+    tx.transactionHash = event.transaction.hash
+    tx.save()
+  }
+  return tx
+}
+
+function getOrCreateProtocol(): RafikNS {
+  let protoId = "protocol"
+  let proto = RafikNS.load(protoId)
+  if (proto == null) {
+    proto = new RafikNS(protoId)
+    proto.save()
+  }
+  return proto
+}
 
 export function handleCreatedName(event: CreatedNameEvent): void {
-  let entity = new CreatedName(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.username = event.params.username
-  entity.userAddress = event.params.userAddress
-  entity.imageURL = event.params.imageURL
+  let tx = getOrCreateTransaction(event)
+  let proto = getOrCreateProtocol()
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let username = event.params.username
+  let id = username
 
-  entity.save()
+  let user = CreatedName.load(id)
+  if (user == null) {
+    user = new CreatedName(id)
+    user.username = username
+    user.userAddress = event.params.userAddress
+    user.imageURL = event.params.imageURL
+    user.transaction = tx.id
+    user.protocol = proto.id
+    user.save()
+  }
 }
 
 export function handleMessaging(event: MessagingEvent): void {
-  let entity = new Messaging(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.reciever = event.params.reciever
-  entity.messageContent = event.params.messageContent
+  let tx = getOrCreateTransaction(event)
+  let proto = getOrCreateProtocol()
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let id = event.transaction.hash.concatI32(event.logIndex.toI32())
+  let message = new Messaging(id)
 
-  entity.save()
+  message.sender = event.params.sender
+  message.reciever = event.params.reciever
+  message.messageContent = event.params.messageContent
+  message.transaction = tx.id
+  message.protocol = proto.id
+
+  let senderUser = CreatedName.load(event.params.sender)
+  if (senderUser != null) {
+    message.senderUser = senderUser.id
+  }
+
+  let receiverUser = CreatedName.load(event.params.reciever)
+  
+  if (receiverUser != null) {
+    message.receiverUser = receiverUser.id
+  }
+
+  message.save()
 }
