a1e78e03bdbb88bf93a9a36c53850ec87ec62e94
Biokes <106444765+Biokes@users.noreply.github.com>
2025-09-19 10:51:57 +0100

    update code

--- subgraph/src/rafik-ns.ts
+++ subgraph/src/rafik-ns.ts
@@ -70,86 +70,86 @@ export function handleMessaging(event: MessagingEvent): void {
 }
 
 
-import {
-  CreatedName as CreatedNameEvent,
-  Messaging as MessagingEvent,
-} from "../generated/RafikNS/RafikNS"
-import { Bytes, ethereum } from "@graphprotocol/graph-ts"
-import {
-  CreatedName,
-  Messaging,
-  RafikNS,
-  Transaction,
-} from "../generated/schema"
+// import {
+//   CreatedName as CreatedNameEvent,
+//   Messaging as MessagingEvent,
+// } from "../generated/RafikNS/RafikNS"
+// import { Bytes, ethereum } from "@graphprotocol/graph-ts"
+// import {
+//   CreatedName,
+//   Messaging,
+//   RafikNS,
+//   Transaction,
+// } from "../generated/schema"
 
-function getOrCreateTransaction(event: ethereum.Event): Transaction {
-  let txId = event.transaction.hash.toHexString()
-  let tx = Transaction.load(txId)
-  if (tx == null) {
-    tx = new Transaction(txId)
-    tx.blockNumber = event.block.number
-    tx.blockTimestamp = event.block.timestamp
-    tx.transactionHash = event.transaction.hash
-    tx.save()
-  }
-  return tx
-}
+// function getOrCreateTransaction(event: ethereum.Event): Transaction {
+//   let txId = event.transaction.hash.toHexString()
+//   let tx = Transaction.load(txId)
+//   if (tx == null) {
+//     tx = new Transaction(txId)
+//     tx.blockNumber = event.block.number
+//     tx.blockTimestamp = event.block.timestamp
+//     tx.transactionHash = event.transaction.hash
+//     tx.save()
+//   }
+//   return tx
+// }
 
-function getOrCreateProtocol(): RafikNS {
-  let protoId = "protocol"
-  let proto = RafikNS.load(protoId)
-  if (proto == null) {
-    proto = new RafikNS(protoId)
-    proto.contractAddress = Bytes.fromHexString("0x305F599fbCd667dbb9ca28960751430A1e8Fc3Ad")
-    proto.save()
-  }
-  return proto
-}
+// function getOrCreateProtocol(): RafikNS {
+//   let protoId = "protocol"
+//   let proto = RafikNS.load(protoId)
+//   if (proto == null) {
+//     proto = new RafikNS(protoId)
+//     proto.contractAddress = Bytes.fromHexString("0x305F599fbCd667dbb9ca28960751430A1e8Fc3Ad")
+//     proto.save()
+//   }
+//   return proto
+// }
 
-export function handleCreatedName(event: CreatedNameEvent): void {
-  let tx = getOrCreateTransaction(event)
-  let proto = getOrCreateProtocol()
+// export function handleCreatedName(event: CreatedNameEvent): void {
+//   let tx = getOrCreateTransaction(event)
+//   let proto = getOrCreateProtocol()
 
-  let username = event.params.username
-  let id = username
+//   let username = event.params.username
+//   let id = username
 
-  let user = CreatedName.load(id)
-  if (user == null) {
-    user = new CreatedName(id)
-    user.username = username
-    user.userAddress = event.params.userAddress
-    user.imageURL = event.params.imageURL
-    user.transaction = tx.id
-    user.protocol = proto.id
-      user.contractAddress = event.address;
-    user.save()
-  }
-}
+//   let user = CreatedName.load(id)
+//   if (user == null) {
+//     user = new CreatedName(id)
+//     user.username = username
+//     user.userAddress = event.params.userAddress
+//     user.imageURL = event.params.imageURL
+//     user.transaction = tx.id
+//     user.protocol = proto.id
+//       user.contractAddress = event.address;
+//     user.save()
+//   }
+// }
 
-export function handleMessaging(event: MessagingEvent): void {
-  let tx = getOrCreateTransaction(event)
-  let proto = getOrCreateProtocol()
+// export function handleMessaging(event: MessagingEvent): void {
+//   let tx = getOrCreateTransaction(event)
+//   let proto = getOrCreateProtocol()
 
-  let id = event.transaction.hash.concatI32(event.logIndex.toI32())
-  let message = new Messaging(id)
+//   let id = event.transaction.hash.concatI32(event.logIndex.toI32())
+//   let message = new Messaging(id)
 
-  message.sender = event.params.sender
-  message.reciever = event.params.reciever
-  message.messageContent = event.params.messageContent
-  message.transaction = tx.id
-  message.protocol = proto.id
-  message.contractAddress = event.address;
+//   message.sender = event.params.sender
+//   message.reciever = event.params.reciever
+//   message.messageContent = event.params.messageContent
+//   message.transaction = tx.id
+//   message.protocol = proto.id
+//   message.contractAddress = event.address;
 
-  let senderUser = CreatedName.load(event.params.sender)
-  if (senderUser != null) {
-    message.senderUser = senderUser.id
-  }
+//   let senderUser = CreatedName.load(event.params.sender)
+//   if (senderUser != null) {
+//     message.senderUser = senderUser.id
+//   }
 
-  let receiverUser = CreatedName.load(event.params.reciever)
+//   let receiverUser = CreatedName.load(event.params.reciever)
   
-  if (receiverUser != null) {
-    message.receiverUser = receiverUser.id
-  }
+//   if (receiverUser != null) {
+//     message.receiverUser = receiverUser.id
+//   }
 
-  message.save()
-}
+//   message.save()
+// }
